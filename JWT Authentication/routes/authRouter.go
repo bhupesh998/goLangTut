944ecdb1d95a601548b4b610923f84681c1f88b4
bhupesh998 <bhupeshchoudhary998@gmail.com>
2024-01-28 14:29:39 +0530

    added changes

---
+++ JWT Authentication/routes/authRouter.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	controller "github.com/bhupesh998/golang-jwt/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("users/login", controller.Login())
+}
---
+++ JWT Authentication/routes/userRouter.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	controller "github.com/bhupesh998/golang-jwt/controllers"
+	"github.com/bhupesh998/golang-jwt/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
